40c1330f1f2ed9de05077cc64fd072e981d9c459
Erik Scow <erikscow@gmail.com>
2020-03-20 19:02:40 -0700

    Created initial file structure. Created the class component that calls the api.

---
+++ client/src/components/Players.js
@@ -0,0 +1,36 @@
+import React from 'react'
+import axios from 'axios'
+
+import PlayerCard from './PlayerCard'
+
+class Players extends React.Component {
+    constructor() {
+        super()
+        this.state = {
+            players: []
+        }
+    }
+
+    componentDidMount() {
+        axios.get('http://localhost:5000/api/players')
+            .then(res => {
+                this.setState({players: res.data})
+                console.log('Players.js: CDM: axios.get.then: ', this.state.players)
+            })
+        
+    }
+
+    render() {
+        return (
+            <div className='players'>
+                {this.state.players.map((player, i) => {
+                    return(
+                        <PlayerCard key={i} player={player}/>
+                    )
+                })}
+            </div>
+        )
+    }
+}
+
+export default Players
